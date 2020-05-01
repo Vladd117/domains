52fe2dd265994cdc25a336e0332a88a87d2f9bde
vladd117 <vladd117@gmail.com>
2020-05-01 15:38:42 +0300

    yoga slider

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -37,7 +37,7 @@ window.addEventListener('DOMContentLoaded',
 
         // Таймер! ---------------------------------->
 
-        let deadline = '2020-05-26';
+        let deadline = '2020-05-3';
 
         function getTimeRemainig(endTime) {
             let t = Date.parse(endTime) - Date.parse(new Date()) - 3 * 60 * 60 * 1000,
@@ -135,4 +135,48 @@ window.addEventListener('DOMContentLoaded',
                 input[i].value = '';
             }
         });
+
+        //Слайдер -------------------------------------------------->
+
+        let slideIndex = 1,
+            slides = document.querySelectorAll('.slider-item'),
+            prev = document.querySelector('.prev'),
+            next = document.querySelector('.next'),
+            dotsWrap = document.querySelector('.slider-dots'),
+            dots = document.querySelectorAll('.dot');
+        showSlides(slideIndex);
+
+        function showSlides(n) {
+            if (n > slides.length) { slideIndex = 1; }
+            if (n < 1) { slideIndex = slides.length; }
+
+            slides.forEach((item) => {
+                item.style.display = 'none';
+            });
+            dots.forEach((item) => {
+                item.classList.remove('dot-active');
+            });
+            slides[slideIndex - 1].style.display = 'block';
+            dots[slideIndex - 1].classList.add('dot-active');
+        }
+
+        function plusSlide(n) {
+            showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+        prev.addEventListener('click', () => { plusSlide(-1); });
+        next.addEventListener('click', () => { plusSlide(1); });
+
+        dotsWrap.addEventListener('click', (event) => {
+            for (let i = 0; i < dots.length; i++) {
+                if (event.target.classList.contains('dot') && event.target == dots[i]) {
+                    currentSlide(i + 1);
+                }
+            }
+        });
+
+
     });
