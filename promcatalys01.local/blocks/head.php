577d32dfdf34cea6186adb9250e47ddcd491d70d
vladd117 <vladd117@gmail.com>
2020-05-18 20:04:02 +0300

    safari link correct

--- promcatalys01.local/blocks/head.php
+++ promcatalys01.local/blocks/head.php
@@ -1,14 +1,14 @@
-
-<meta charset="UTF-8"></meta>
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Promcatalys</title>
-<link rel="stylesheet" type="text/css" href="./css/style.css">
-<link rel="stylesheet" type="text/css" href="./css/leftside-btn.css">
-<link rel="stylesheet" type="text/css" href="./css/prodlist.css">
-<link rel="stylesheet" type="text/css" href="./css/product.css">
-<link rel="stylesheet" type="text/css" href="./css/intro.css">
-<link rel="stylesheet" type="text/css" href="./css/back.css">
-<script src="./js/closes.js"></script>
-
-<?php include('./db/connectdb.php'); ?>
+<meta charset="UTF-8"></meta>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+<title>Promcatalys</title>
+<link rel="stylesheet" type="text/css" href="./css/style.css">
+<link rel="stylesheet" type="text/css" href="./css/leftside-btn.css">
+<link rel="stylesheet" type="text/css" href="./css/prodlist.css">
+<link rel="stylesheet" type="text/css" href="./css/product.css">
+<link rel="stylesheet" type="text/css" href="./css/intro.css">
+<link rel="stylesheet" type="text/css" href="./css/back.css">
+<script src="./js/closes.js"></script>
+
+<?php include('./db/connectdb.php'); ?>
