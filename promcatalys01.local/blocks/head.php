1cd669a8fb8bdefdf822d39ba9f90c06ad84f99f
vladd117 <vladd117@gmail.com>
2020-06-03 17:55:15 +0300

    site add to hosting!

--- promcatalys01.local/blocks/head.php
+++ promcatalys01.local/blocks/head.php
@@ -6,6 +6,21 @@
 
 <meta name="format-detection" content="telephone=no">
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(64605622, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/64605622" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 <title>Promcatalys</title>
 <link rel="stylesheet" type="text/css" href="./css/back.css">
 <link rel="stylesheet" type="text/css" href="./css/style.css">
@@ -14,7 +29,7 @@
 <link rel="stylesheet" type="text/css" href="./css/product.css">
 <link rel="stylesheet" type="text/css" href="./css/intro.css">
 
-
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 
 <script src="./js/closes.js"></script>
 
