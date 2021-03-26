02f61aa1c6e62f56f5cbec71d2b8680f6026fd37
Vladislav <vladd117@gmail.com>
2021-03-26 13:38:06 +0300

    fixing something

--- promcatalys01.local/blocks/leftsidebar.php
+++ promcatalys01.local/blocks/leftsidebar.php
@@ -1,66 +1,68 @@
-
 <div class="leftside">
-<div class="allbuttons">
-    <!-- Animated button -->
-    <div class="lbtn-container">
-        <button class="lbtn-learn-more">
-            <div class="lbtn-circle">
-                <div class="lbtn-button-icon lbtn-rotate-450">></div>
-            </div>
-            <div class="lbtn-button-text lbtext">Продукция</div>
+    <div class="allbuttons">
+        <!-- Animated button -->
+        <div class="lbtn-container">
+            <button class="lbtn-learn-more">
+                <div class="lbtn-circle">
+                    <div class="lbtn-button-icon lbtn-rotate-450">></div>
+                </div>
+                <div class="lbtn-button-text lbtext">Продукция</div>
 
-        </button>
-        <a href="./prodlist.php?category=rif&title=Катализаторы риформинга#anchor">
-            <div class="lbtn-sub-button"><button>Катализаторы риформинга</button></div>
-        </a>
-        <a href="./prodlist.php?category=izom&title=Катализаторы изомеризации#anchor">
-            <div class="lbtn-sub-button"><button>Катализаторы изомеризации</button></div>
-        </a>
-        <a href="./prodlist.php?category=go&title=Катализаторы гидроочистки#anchor">
-            <div class="lbtn-sub-button"><button>Катализаторы гидроочистки</button></div>
-        </a>
-        <a href="./prodlist.php?category=gig&title=Катализаторы гидрирования и гидрокрекинга#anchor">
-            <div class="lbtn-sub-button"><button>Катализаторы гидрирования и гидрокрекинга</button></div>
-        </a>
-        <a href="./prodlist.php?category=klaus&title=Катализаторы процесса Клауса#anchor">
-            <div class="lbtn-sub-button"><button>Катализаторы процесса Клауса</button></div>
-        </a>
-        <a href="./prodlist.php?category=eco&title=Экологические катализаторы#anchor">
-            <div class="lbtn-sub-button"><button>Экологические катализаторы</button></div>
-        </a>
-        <a href="./prodlist.php?category=nos&title=Носители для катализаторов#anchor">
-            <div class="lbtn-sub-button"><button>Носители для катализаторов</button></div>
-        </a>
-        <a href="./prodlist.php?category=adsorb&title=Адсорбенты и осушители#anchor">
-            <div class="lbtn-sub-button"><button>Адсорбенты и осушители</button></div>
-        </a>
-        
-    </div>
+            </button>
+            <a href="./prodlist.php?category=rif&title=Катализаторы риформинга#anchor">
+                <div class="lbtn-sub-button"><button>Катализаторы риформинга</button></div>
+            </a>
+            <a href="./prodlist.php?category=izom&title=Катализаторы изомеризации#anchor">
+                <div class="lbtn-sub-button"><button>Катализаторы изомеризации</button></div>
+            </a>
+            <a href="./prodlist.php?category=go&title=Катализаторы гидроочистки#anchor">
+                <div class="lbtn-sub-button"><button>Катализаторы гидроочистки</button></div>
+            </a>
+            <a href="./prodlist.php?category=gig&title=Катализаторы гидрирования и гидрокрекинга#anchor">
+                <div class="lbtn-sub-button"><button>Катализаторы гидрирования и гидрокрекинга</button></div>
+            </a>
+            <a href="./prodlist.php?category=klaus&title=Катализаторы процесса Клауса#anchor">
+                <div class="lbtn-sub-button"><button>Катализаторы процесса Клауса</button></div>
+            </a>
+            <a href="./prodlist.php?category=kzs&title=Катализаторы защитного слоя#anchor">
+                <div class="lbtn-sub-button"><button>Катализаторы защитного слоя</button></div>
+            </a>
+            <a href="./prodlist.php?category=eco&title=Экологические катализаторы#anchor">
+                <div class="lbtn-sub-button"><button>Экологические катализаторы</button></div>
+            </a>
+            <!-- <a href="./prodlist.php?category=nos&title=Носители для катализаторов#anchor">
+                <div class="lbtn-sub-button"><button>Носители для катализаторов</button></div>
+            </a> -->
+            <a href="./prodlist.php?category=adsorb&title=Адсорбенты и осушители#anchor">
+                <div class="lbtn-sub-button"><button>Адсорбенты и осушители</button></div>
+            </a>
 
-    <!-- Animated button -->
-    <div class="lbtn-container">
-        <button class="lbtn-learn-more">
-            <div class="lbtn-circle">
-                <div class="lbtn-button-icon lbtn-rotate-450">></div>
-            </div>
-            <div class="lbtn-button-text2 lbtext">Технический сервис</div>
+        </div>
 
-        </button>
-        <?php
-        $sql = 'SELECT * FROM `articles` WHERE `location` = ?';
-        $location = 'techservice';
-        $query = $pdo->prepare($sql);
-        $query->execute(array($location));
-        while ($row = $query->fetch(PDO::FETCH_OBJ)) {
-            echo "<a href='./articles.php?id=$row->id&location=$row->location#anchor'><div class='lbtn-sub-button'><button>$row->name</button></div></a>";
-        }
+        <!-- Animated button -->
+        <div class="lbtn-container">
+            <button class="lbtn-learn-more">
+                <div class="lbtn-circle">
+                    <div class="lbtn-button-icon lbtn-rotate-450">></div>
+                </div>
+                <div class="lbtn-button-text2 lbtext">Технический сервис</div>
 
-        ?>
+            </button>
+            <?php
+            $sql = 'SELECT * FROM `articles` WHERE `location` = ?';
+            $location = 'techservice';
+            $query = $pdo->prepare($sql);
+            $query->execute(array($location));
+            while ($row = $query->fetch(PDO::FETCH_OBJ)) {
+                echo "<a href='./articles.php?id=$row->id&location=$row->location#anchor'><div class='lbtn-sub-button'><button>$row->name</button></div></a>";
+            }
 
-    </div>
+            ?>
 
-    <!-- Animated button -->
-    <!-- <div class="lbtn-container">
+        </div>
+
+        <!-- Animated button -->
+        <!-- <div class="lbtn-container">
         <button class="lbtn-learn-more">
             <div class="lbtn-circle">
                 <div class="lbtn-button-icon lbtn-rotate-450">></div>
@@ -81,35 +83,35 @@
         ?>
 
     </div> -->
-    <!-- Animated button -->
-    <div class="lbtn-container">
-        <button class="lbtn-learn-more">
-            <div class="lbtn-circle">
-                <div class="lbtn-button-icon lbtn-rotate-450">></div>
-            </div>
-            <div class="lbtn-button-text2 lbtext">Аналитическая база</div>
+        <!-- Animated button -->
+        <div class="lbtn-container">
+            <button class="lbtn-learn-more">
+                <div class="lbtn-circle">
+                    <div class="lbtn-button-icon lbtn-rotate-450">></div>
+                </div>
+                <div class="lbtn-button-text2 lbtext">Аналитическая база</div>
 
-        </button>
-        <?php
-        $sql = 'SELECT * FROM `articles` WHERE `location` = ?';
-        $location = 'analytics';
-        $query = $pdo->prepare($sql);
-        $query->execute(array($location));
+            </button>
+            <?php
+            $sql = 'SELECT * FROM `articles` WHERE `location` = ?';
+            $location = 'analytics';
+            $query = $pdo->prepare($sql);
+            $query->execute(array($location));
 
-        while ($row = $query->fetch(PDO::FETCH_OBJ)) {
-            echo "<a href='./articles.php?id=$row->id&location=$row->location#anchor'><div class='lbtn-sub-button'><button>$row->name</button></div></a>";
-        }
+            while ($row = $query->fetch(PDO::FETCH_OBJ)) {
+                echo "<a href='./articles.php?id=$row->id&location=$row->location#anchor'><div class='lbtn-sub-button'><button>$row->name</button></div></a>";
+            }
 
-        ?>
+            ?>
 
-    </div>
-    <!-- <div class="lbtn-sub-button"><button>$row->name</button></div>
+        </div>
+        <!-- <div class="lbtn-sub-button"><button>$row->name</button></div>
         <div class="lbtn-sub-button"><button>Изомеризации</button></div>
         <div class="lbtn-sub-button"><button>Клауса</button></div>
         <div class="lbtn-sub-button"><button>Экологические</button></div>
         <div class="lbtn-sub-button"><button>Носители</button></div> -->
-    <!-- <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>&nbsp; -->
-       </div>
-   
+        <!-- <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>&nbsp; -->
+    </div>
+
     <script src="./js/lsbutton.js"></script>
 </div>
